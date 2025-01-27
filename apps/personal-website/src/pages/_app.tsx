7e90a4b73ffb93a2daa1c14641fa5a3e807f18cf
Michael O'Connell <michael@shineweddinginvitations.com>
2025-01-27 10:44:31 -0500

    init

--- apps/personal-website/src/pages/_app.tsx
+++ apps/personal-website/src/pages/_app.tsx
@@ -1,13 +1,18 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
-
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { CssBaseline } from '@mui/material';
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to personasl-website!</title>
+        <title>Welcome to personal-website!</title>
       </Head>
+      <CssBaseline />
       <main className="app">
         <Component {...pageProps} />
       </main>
